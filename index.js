3c1e027fbcddceeb0fa12026af68fa3c6f526edb
Juan Cruz Diaz <juancruzdiaz12@gmail.com>
2022-10-07 20:03:54 -0300

    Agregué un carrito, pero tiene dos botones rotos (el 2 y el 3 de las remeras)

--- index.js
+++ index.js
@@ -6,9 +6,31 @@ let iDatos;
 let bIngresar;
 let IdDivPedirDatos;
 
+/* CONSTANTES */
+
+const NOMBREPRODUCTOS = []
+
+const carrito = [];
+
+const productosJS = []
+
+/* MI CONSTRUCTOR */
+
+class Producto{
+     constructor (id, nombreProducto, precio){
+          this.id = id;
+          this.nombreProducto = nombreProducto;
+          this.precio = precio;
+     }
+}
+
+
+
+
+
 /* VARIABLES AL DOM */
 
-function incializarElementos (){
+function inicializarElementos (){
      contenedorPedirDatos = document.getElementById("contenedorPedirDatos")
      fPedirDatos = document.getElementById("pedirDatos")
      iDatos = document.getElementById("datos")
@@ -16,35 +38,26 @@ function incializarElementos (){
      IdDivPedirDatos = document.getElementById("IdDivPedirDatos")
 }
 
-/* EVENTOS */
-
-// function inicializarEventos() {
-//      contenedorPedirDatos.onsubmit = (e) => contenedorPedirDatos(e);
-// }
-
-function validarIdentificacion() {
-     
-     // contenedorPedirDatosV = contenedorPedirDatos.value;
-     // fPedirDatosV = fPedirDatos.value;
-     // iDatos = iDatos.value;
-     // bIngresarV = bIngresar.value;
-     // console.log(iDatos.value)
-     
-}
 
 function asignarClaseDFlexNone(input){
      if(input !== ""){
           IdDivPedirDatos.className = "display-flex"
           crearLabel()
           mostrarLista()
+          eventoBotones();
      }
      else{
           alert("No ingresó un nombre")
      }
 }
 
+function eventoBotones(){
+     botonMostrarProductos()
+}
+
 function crearLabel(){
-     contenedorPedirDatos.innerHTML = `<h4 class="m-3 tamañoFuente ">Hola <b> ${iDatos.value}</b>, que desea llevar? </4>`
+     contenedorPedirDatos.innerHTML = `<h2 class="m-3 tamanioFuente ">Bienvenido <b> ${iDatos.value}</b> </2>
+     <h1>¿Que va a llevar?</h1>`
 } 
 
 function mostrarLista(){
@@ -56,49 +69,188 @@ function mostrarLista(){
 
 
 
+
+/* TRAER */
+
+
+function traerH5() {
+     let titulos = document.getElementsByClassName("card-body")
+     
+      for (let i = 0 ; i<titulos.length ; i++){     
+          let identificador = i + 1
+          let titulo = titulos.item(i).firstElementChild.innerText
+          let precio = titulos.item(i).lastElementChild.innerText
+          precio = parseFloat(precio.slice(2))
+          productosJS.push(new Producto(identificador, titulo, precio))
+          
+          }
+     
+} 
+
+function traerH6(){
+     const H6 = document.getElementsByTagName("h6")
+
+     for(item of H6) {
+          item.classList.add("tamanioFuentePrecio")
+     }
+}
+
+function traerP() {
+     const P = document.getElementsByTagName("p")
+
+     for (item of P) {
+          item.classList.add("tamanioFuente")
+     }
+}
+
+function traerIds(){
+     let botones = document.getElementsByTagName("button")
+     let d = 0
+     for (let i = 0 ; i < botones.length ; i++){
+          if(i>3){
+               let precioProducto = productosJS[d].precio
+               let nombreProducto = productosJS[d].nombreProducto
+               let valorId = botones.item(i).id
+               botonComprar(valorId, precioProducto, nombreProducto)
+               d++;
+          }
+          
+     }
+     
+}
+
+/* FUNCION GENERAL PARA TRAER */
+
+function traerTags() {
+     traerH5();
+     traerH6();
+     traerP();
+     traerIds();
+}
+
+
+function botonComprar(valorId, precio, nombreProducto){
+     let boton = document.getElementById(valorId)
+     boton.onclick = () => {
+          NOMBREPRODUCTOS.push(nombreProducto)
+          carrito.push(precio)
+          imprimirCarrito()
+     }
+}
+
+
+ function imprimirCarrito(){
+     let contenedor = document.getElementById("contenedorCarrito")
+     let sum = 0;
+     let lis = ""
+     for (let i = 0; i < carrito.length; i++) 
+     {
+          sum += carrito[i];
+          lis += `<li>${NOMBREPRODUCTOS[i]}: $${carrito[i]} </li>`
+     }
+     contenedor.innerHTML = `
+     <h4 class="tamanioFuente">TU CARRITO</h4>
+
+          <div id="card2">
+             <ul>${lis}</ul>
+             <p><b>Precio Total: $${sum}</b></p>
+          </div>`
+ }
+ 
+
+const eliminarItem = (id) => {
+     let borrar = carrito.find((producto) => producto.id === id)
+     let indice = carrito.indexOf(borrar)
+     carrito.splice(indice, 1)
+     renderCarrito()
+     calcularTotal()
+}
+
+const divPrecio = document.querySelector("precioTotal")
+
+calcularTotal = () => {
+     let cont = 0
+     carrito.forEach((pre) => {
+          cont += pre.precio
+     })
+
+     divPrecio.innerHTML = cont
+}
+
+
+function botonMostrarProductos(){
+     let eliminarRemeras = document.getElementById("contenedorRemeras");
+     let eliminarCamisas = document.getElementById("contenedorCamisas");
+     let eliminarPantalones = document.getElementById("contenedorPantalones");
+     let btnRemeras = document.getElementById("Remeras")
+     
+     
+     btnRemeras.onclick = () => {
+          
+          eliminarRemeras.classList.remove("display-flex")
+          eliminarCamisas.className = "display-flex"
+          eliminarPantalones.className = "display-flex"
+
+
+     }
+
+     let btnCamisas = document.getElementById("Camisas")
+
+     btnCamisas.onclick = () => {
+          eliminarCamisas.classList.remove("display-flex")
+          eliminarRemeras.className = "display-flex"
+          eliminarPantalones.className = "display-flex"
+     }
+
+
+     let btnPantalones = document.getElementById("Pantalones")
+
+     btnPantalones.onclick = () => {
+          eliminarPantalones.classList.remove("display-flex")
+          eliminarCamisas.className = "display-flex"
+          eliminarRemeras.className = "display-flex"
+     }
+     
+}
+
+
 function agregar(){
 
-     const productosId = ["Remeras", "Camisas", "Sweters", "Buzos", "Pantalones", "Bermudas"]
+     const productosId = ["Remeras", "Camisas", "Pantalones"]
  
-     let divProductos = document.getElementById("divProductos")
+     let divProductos = document.getElementById("divCentrados")
  
      for(const producto of productosId){
          let li = document.createElement("li")
-         li.innerHTML = `<button class="tamañoFuente display-flex botones" id="producto">${producto}</button>`
+         li.innerHTML = `<button class="tamanioFuente botones" id="${producto}">${producto}</button>`
          divProductos.appendChild(li)
      }
  
  }
  
- 
+
 
 
 function inicializarEventos() {
      fPedirDatos.addEventListener("submit", e => {
          e.preventDefault()
          
-         validarIdentificacion()
          asignarClaseDFlexNone(iDatos.value)
 
          
      })
      fPedirDatos.reset()
  }
+ 
 
-
-
-
-/* function presionarBoton (){
-     contenedorPedirDatos.onclick =
-} */
 
 
 function main() {     
-     incializarElementos();
+     inicializarElementos();
      inicializarEventos();
-     agregar()
-     
-     
+     traerTags();
+     agregar();
+  
 }
 
 main();
